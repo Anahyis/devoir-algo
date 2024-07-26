be25d2754387fc21d40e5ae6f612f9001d7d2805
Anahyis <anahyisg@gmail.com>
2024-07-26 16:40:39 +0200

    Fin mais pas d'étape 4, sorry !

--- exercice.php
+++ exercice.php
@@ -7,7 +7,7 @@ while ($budget > $nul) {
     $question = readline("Recette ou Dépense ? \n");
     switch($question) {
         case "Recette";
-        $R = readline("Quel est le montant de la recette ? \n");
+        $R = (float) readline("Quel est le montant de la recette ? \n");
         $add = 0;
         $add = $budget + $R;
         echo "Ton budget est de : " . $add . "\n";
@@ -16,16 +16,16 @@ while ($budget > $nul) {
         break;
 
         case "Dépense";
-        $D = readline("Quel est le montant de la dépense ? \n");
+        $D = (float) readline("Quel est le montant de la dépense ? \n");
         if ($D < $budget) {
             $less = $budget - $D;
             $percentage = $D / $budget * 100;
             echo "Ton budget est maintenant de : " . $less . "\n" . "Cela représente une dépense de " . $percentage . "% de ton budget" . "\n";
+            $newbudget = $less;
+            $budget = $newbudget;
         } else if ($D >= $budget) { 
             echo "Erreur. Dépense trop élevée ! \n";
         }
-        $newbudget = $less;
-        $budget = $newbudget;
         break;
     }
 }
