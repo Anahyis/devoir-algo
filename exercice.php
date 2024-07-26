c15679c0e662eaed5c44adfdb06bee5f6316a689
Anahyis <anahyisg@gmail.com>
2024-07-26 16:33:58 +0200

    Etape 3 modifications

--- exercice.php
+++ exercice.php
@@ -5,36 +5,32 @@ $nul = 0;
 
 while ($budget > $nul) {
     $question = readline("Recette ou Dépense ? \n");
-    $R = "Recette";
+    switch($question) {
+        case "Recette";
         $R = readline("Quel est le montant de la recette ? \n");
         $add = 0;
         $add = $budget + $R;
-        readline("Ton budget est de : " . $add);
-    $D = "Dépense";
+        echo "Ton budget est de : " . $add . "\n";
+        $newbudget = $add;
+        $budget = $newbudget;
+        break;
+
+        case "Dépense";
         $D = readline("Quel est le montant de la dépense ? \n");
         if ($D < $budget) {
             $less = $budget - $D;
             $percentage = $D / $budget * 100;
-            echo "Ton budget est maintenant de : " . $less . "\n" . "Cela représente une dépense de " . $percentage . "% de ton budget";
+            echo "Ton budget est maintenant de : " . $less . "\n" . "Cela représente une dépense de " . $percentage . "% de ton budget" . "\n";
         } else if ($D >= $budget) { 
             echo "Erreur. Dépense trop élevée ! \n";
         }
+        $newbudget = $less;
+        $budget = $newbudget;
+        break;
+    }
 }
 
 
 
 
-
-
-
-// while ($budget > $nul) {
-//     $question = readline("Recette ou Dépense ? \n");
-//     $R = "Recette";
-//         $R = readline("Quel est le montant de la recette ? \n");
-//         $add = 0;
-//         $add = $budget + $R;
-//         readline("Ton budget est de : " . $add);
-//     $D = "Dépense";
-//         $D = readline("Quel est le montant de la dépense ? \n");
-// }
 ?>
