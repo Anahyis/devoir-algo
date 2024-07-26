6987dd4f29ac8f3462022e39facfe26b1aefeab4
Anahyis <anahyisg@gmail.com>
2024-07-26 15:57:16 +0200

    Etape 3

--- exercice.php
+++ exercice.php
@@ -4,8 +4,37 @@ $budget = (int) readline("Saisie ton budget initial");
 $nul = 0;
 
 while ($budget > $nul) {
-    
+    $question = readline("Recette ou Dépense ? \n");
+    $R = "Recette";
+        $R = readline("Quel est le montant de la recette ? \n");
+        $add = 0;
+        $add = $budget + $R;
+        readline("Ton budget est de : " . $add);
+    $D = "Dépense";
+        $D = readline("Quel est le montant de la dépense ? \n");
+        if ($D < $budget) {
+            $less = $budget - $D;
+            $percentage = $D / $budget * 100;
+            echo "Ton budget est maintenant de : " . $less . "\n" . "Cela représente une dépense de " . $percentage . "% de ton budget";
+        } else if ($D >= $budget) { 
+            echo "Erreur. Dépense trop élevée ! \n";
+        }
 }
 
 
+
+
+
+
+
+// while ($budget > $nul) {
+//     $question = readline("Recette ou Dépense ? \n");
+//     $R = "Recette";
+//         $R = readline("Quel est le montant de la recette ? \n");
+//         $add = 0;
+//         $add = $budget + $R;
+//         readline("Ton budget est de : " . $add);
+//     $D = "Dépense";
+//         $D = readline("Quel est le montant de la dépense ? \n");
+// }
 ?>
